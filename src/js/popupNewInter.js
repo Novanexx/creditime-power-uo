186e1bd8fc2ca52284ed3a20775907f2400ea745
gaetan mbumba <gaetan.mbumba@gmail.com>
2021-05-17 01:48:03 +0200

    Popup new intervention test retour button

--- src/js/popupNewInter.js
+++ src/js/popupNewInter.js
@@ -1,4 +1,4 @@
-var t = window.TrelloPowerUp.iframe();
+//var t = window.TrelloPowerUp.iframe();
 
 document.getElementById("validate").addEventListener("click", function(event){
     console.log("Valider")
