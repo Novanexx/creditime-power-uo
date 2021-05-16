7e657fd753f08e07eca1da51ea7fb0a59e807717
gaetan mbumba <gaetan.mbumba@gmail.com>
2021-05-17 01:56:35 +0200

    Popup new intervention test retour des inputs

--- src/js/popupNewInter.js
+++ src/js/popupNewInter.js
@@ -1,5 +1,9 @@
 //var t = window.TrelloPowerUp.iframe();
 
 document.getElementById("validate").addEventListener("click", function(event){
-    console.log("Valider")
+
+    var temps = document.getElementById("temps").value;
+    var description = document.getElementById("description").value;
+    console.log(temps);
+    console.log(description);
 });
